2689baef00addef4356e6b241c5af239f502301a
Willmer Barahona <wbarahona@live.com>
2014-08-20 12:44:41 -0600

    set, update, removes data from firebase API
    I have added the set, update, remove button and we can update the data
    in the firebase API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,51 @@
+/*global Firebase*/
 'use strict';
 
 /**
  * @ngdoc function
- * @name angularFirebaseApp.controller:MainCtrl
+ * @name angularFirebaseApp.controller:MainController
  * @description
- * # MainCtrl
+ * # MainController
  * Controller of the angularFirebaseApp
  */
 angular.module('angularFirebaseApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainController', function ($scope) {
+        // Declaring global variables for this controller
+        // ------------------------------------------------------------------------
+            var scope = $scope;
+            // Firebase API for this app
+            var rootRef = new Firebase('https://vivid-heat-1154.firebaseio.com/');
+            var messagesRef = rootRef.child('messages');
+
+        // Injecting into the scope var elements needed in the view
+        // ------------------------------------------------------------------------
+            scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+
+        // Declaring  a function that pushes a hardcoded message into the
+        // firebase API
+        // ------------------------------------------------------------------------
+            scope.setMessage = function () {
+                messagesRef.set({
+                    user: 'Willmer',
+                    message: 'Yo! City Wok seems to be closed today according to swarm app! :('
+                });
+            };
+
+        // Declaring  a function that pushes a hardcoded message into the
+        // firebase API
+        // ------------------------------------------------------------------------
+            scope.updMessage = function () {
+                messagesRef.update({
+                    message: 'Hey Ana! City Wok seems to be closed today according to swarm app! :( :O'
+                });
+            };
+
+            scope.delMessage = function () {
+                messagesRef.remove();
+            };
+
+    });
