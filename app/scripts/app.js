16d35863d4b0de8beb42405e3588fc12c739c533
Willmer Barahona <wbarahona@live.com>
2014-08-20 10:24:36 -0600

    First Commit
    Vanilla scaffold from Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularFirebaseApp
+ * @description
+ * # angularFirebaseApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularFirebaseApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
